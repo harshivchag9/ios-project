c815b9f6e2c71a101274961e8ae8b1e939a296f5
harshivchag9 <63035103+harshivchag9@users.noreply.github.com>
2021-10-10 14:44:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL , "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=54ed1dcef0994aa5b1714a6e97728978");
-  curl_setopt($ch, CURLOPT_POST , 1);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER , 1);
   $res = curl_exec($ch);
   echo $res;
